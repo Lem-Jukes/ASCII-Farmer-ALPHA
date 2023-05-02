0844b80ac307983cf7ea858073b0c85c9bbb6602
Lem-Jukes <lem@jukeltd.com>
2023-05-01 23:07:01 -0400

    Starting to flesh things out - second update

---
+++ main.js
@@ -0,0 +1,43 @@
+// Variables 
+    // Inventory Variables
+        // Currency Variables
+            //  Primary Coin Variable
+        // Tool Variables
+            // Current Tool Variable
+        // Seed Variables
+            // Current Selected Seed Type Variable
+            // Seed Stock Variables
+                // seedType1 count variable
+                // seedType2 count variable
+                // seedType3 count variable
+        // Crop Variables
+            // Crop Stock Variables
+                // cropType1 count variable
+                // cropType2 count variable
+                // cropType3 count variable
+    // Store Variables
+        // Seed Store Variables
+            // seedType1 Price Variable
+            // seedType2 price variable
+            // seedType3 price variable
+        // Crop Store Variables
+            // cropType1 price variable
+            // cropType2 price variable
+            // cropType3 price variable
+    // Field Variables
+        // Field Container Variables
+        // Field Table Variables
+        // Field Pricing Varialbes
+            // Field creaation cost variable
+            // Field expansion cost variable
+
+// Functions
+    // Inventory Functions
+        // Currency Functions
+        // Tool Functions
+        // Seed Functions
+        // Crop Functions
+    // Store Functions
+        // Seed Store Functions
+        // Crop Store Functions
+    // Field Functions
