cf35ed26f7382cdb0d402857936b3baa02a5d833
Lem-Jukes <lem@jukeltd.com>
2023-05-01 23:42:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -28,16 +28,72 @@
         // Field Container Variables
         // Field Table Variables
         // Field Pricing Varialbes
-            // Field creaation cost variable
+            // Field creation cost variable
             // Field expansion cost variable
 
 // Functions
+    // General Functions
+        // Display Functions
+            // Inventory & Store Display Update Functions
+                // Currency Display Updaters
+                    // Primary Coin Display Update
+                // Tool Display Updaters
+                    // Current Tool Selection Display Update
+                // Seed Display Updaters
+                    // Current Seed Type Selection Display Update
+                    // Seed Stock Display Updaters
+                        // currentSeedType count updater
+                        // seedType1 count updater
+                        // seedType2 count updater
+                        // seedType3 count updater
+                // Crop Display Updaters
+                    // cropType1 count updater
+                    // cropType2 count updater
+                    // cropType3 count updater
+            // Field Display Update Functions
     // Inventory Functions
         // Currency Functions
+            // Check Item Cost
+            // Check New Field Cost
+            // Update New Field Cost
+            // Check Field Expansion Cost
+            // Update Field Expansion Cost
         // Tool Functions
+            // Change Tool
+                // Disable the selected tool button
+                // Enable all other tool buttons
         // Seed Functions
+            // Change seed type
+                // Disable the selected seed type button
+                // Enable all other buttons
         // Crop Functions
+            // Actually we dont have any crop functions yet but it felt wrong to leave them out
     // Store Functions
         // Seed Store Functions
+            // Buy seedType1
+                // 1x = 1 primary coin
+                // 5x = 4.5 primary coins
+            // Buy seedType2
+                // 1x = 2 primary coin
+                // 5x = 9 primary coins
+            // Buy seedType3
+                // 1x = 3 primary coin
+                // 5x = 13.5 primary coins
         // Crop Store Functions
+            // Sell cropType1 
+                // 1x = between 2 and 4 primary coins
+                // 10x = Between 20 and 40 primary coins    
+            // Sell cropType2
+                // 1x = between 3 and 5 primary coins
+                // 10x = Between 30 and 50 primary coins  
+            // Sell cropType3
+                // 1x = between 4 and 6 primary coins
+                // 10x = Between 40 and 60 primary coins  
     // Field Functions
+        // Field Creation
+        // Field Expansion
+    // Field Tile Functions
+        // Click Response function
+            // Check field tile state
+            // Check current selected tool
+
